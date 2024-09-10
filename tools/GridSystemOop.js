c8c513484727628fe84d008eba97d8cf6504a84d
80mahd08 <amarimahdi8@gmail.com>
2024-09-10 15:20:04 +0100

    organize more of the code

--- tools/GridSystemOop.js
+++ tools/GridSystemOop.js
@@ -1,27 +1,27 @@
 class GridSystemOop {
-  constructor(numColumns = 1, gapValue) {
-    this.container = {
-      display: "grid",
-      gridTemplateColumns: `repeat(${numColumns}, 1fr)`,
-      gap: gapValue ? `${gapValue}px` : "0px",
-    };
+	constructor(numColumns = 1, gapValue) {
+		this.container = {
+			display: "grid",
+			gridTemplateColumns: `repeat(${numColumns}, 1fr)`,
+			gap: gapValue ? `${gapValue}px` : "0px",
+		};
 
-    this.column = this.column.bind(this);
-  }
+		this.column = this.column.bind(this);
+	}
 
-  column(span, offset = 0) {
-    if (span < 1 || span > 12 || offset < 0 || offset > 11) {
-      throw Error("Invalid column span or offset values");
-    }
+	column(span, offset = 0) {
+		if (span < 1 || span > 12 || offset < 0 || offset > 11) {
+			throw Error("Invalid column span or offset values");
+		}
 
-    this.container.column = {
-      gridColumn: `span ${span} / span ${span}`,
-      marginLeft:
-        offset > 0 ? `calc(${offset} * 1fr + ${offset * 16}px)` : undefined,
-    };
+		this.container.column = {
+			gridColumn: `span ${span} / span ${span}`,
+			marginLeft:
+				offset > 0 ? `calc(${offset} * 1fr + ${offset * 16}px)` : undefined,
+		};
 
-    return this;
-  }
+		return this;
+	}
 }
 
 export default GridSystemOop;
